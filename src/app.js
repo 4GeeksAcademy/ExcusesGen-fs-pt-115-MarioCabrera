446b8d2806fb4cf46f38b49d0dffeb8ff248e818
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-06-13 13:04:59 +0000

    Excusado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let excuse = [];
+  for (let i = 0; i < 4; i++) {
+    switch (i) {
+      case 0:
+        excuse[i] = who[Math.floor(Math.random()*4)]
+        console.log(excuse);
+        
+        break;
+      case 1:
+        excuse[i] = action[Math.floor(Math.random()*4)]
+        console.log(excuse);
+        break;
+      case 2:
+        excuse[i] = what[Math.floor(Math.random()*3)]
+        console.log(excuse);
+        break;
+      case 3:
+        excuse[i] = when[Math.floor(Math.random()*4)]
+        console.log(excuse);
+        break;
+    
+      default:
+        break;
+    }
+    
+  }
+  document.getElementById("Excuses").innerHTML = excuse.join(` `);
 };
